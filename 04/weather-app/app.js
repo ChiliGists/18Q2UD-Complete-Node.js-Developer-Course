c077c089b1cade1fc3f8c29cf6718185be048d84
ChiliDogDaddyO <chilidogdaddyo@gmail.com>
2018-04-15 17:28:48 -0400

    Ch4,31

--- 04/weather-app/app.js
+++ 04/weather-app/app.js
@@ -1,12 +1,29 @@
 const request = require("request");
+const yargs = require("yargs");
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 request(
   {
-    url:
-      "https://maps.googleapis.com/maps/api/geocode/json?address=103%20E%20hypericum%20ln%20greenville%20sc",
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   },
   (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
   }
 );
