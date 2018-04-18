3eaf2245f0a4362c79b6579193eae9b65b14ab08
ChiliDogDaddyO <chilidogdaddyo@gmail.com>
2018-04-17 21:45:28 -0400

    Ch4,34

--- 04/weather-app/app.js
+++ 04/weather-app/app.js
@@ -1,24 +1,43 @@
-const yargs = require("yargs");
-const geocode = require("./geocode/geocode");
+// const yargs = require("yargs");
+// const geocode = require("./geocode/geocode");
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: "address",
-      describe: "Address to fetch weather for",
-      string: true
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: "address",
+//       describe: "Address to fetch weather for",
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias("help", "h").argv;
+
+// // console.log(argv);
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+// 16f35bb1db22d258292b7e6825d60213
+//  https://api.darksky.net/forecast/16f35bb1db22d258292b7e6825d60213/34,-82
+
+const request = require("request");
+
+request(
+  {
+    url: `https://api.darksky.net/forecast/16f35bb1db22d258292b7e6825d60213/34.8742328,-82.33433749999999`,
+    json: true
+  },
+  (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log(`The temperature is ${body.currently.temperature}.`);
+    } else {
+      console.log("Unable to fetch weather.");
     }
-  })
-  .help()
-  .alias("help", "h").argv;
-
-// console.log(argv);
-
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
   }
-});
+);
