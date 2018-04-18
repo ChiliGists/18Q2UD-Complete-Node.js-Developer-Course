ca114dac05f030cc18fdd624d7d24327f9b0daff
ChiliDogDaddyO <chilidogdaddyo@gmail.com>
2018-04-17 22:48:47 -0400

    ch4,35b

--- 04/weather-app/app.js
+++ 04/weather-app/app.js
@@ -15,25 +15,27 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
 
-// console.log(argv);
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(
+      results.latitude,
+      results.longitude,
+      (errorMessage, weatherResults) => {
+        if (errorMessage) {
+          console.log(errorMessage);
+        } else {
+          console.log(
+            `It's currently ${weatherResults.temperature}.  It feels like ${
+              weatherResults.apparentTemperature
+            }.`
+          );
+        }
+      }
+    );
+  }
+});
 
 // lat,lng, callback
-weather.getWeather(
-  34.8742328,
-  -82.33433749999999,
-  (errorMessage, weatherResults) => {
-    if (errorMessage) {
-      console.log(errorMessage);
-    } else {
-      console.log(JSON.stringify(weatherResults, undefined, 2));
-    }
-  }
-);
